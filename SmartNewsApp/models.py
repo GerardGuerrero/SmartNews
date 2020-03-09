2f48fbaca23a9c9c85b9d0990c66d637b1d8d9b9
rafa <rafacucugaz9911@gmail.com>
2020-03-09 13:50:03 +0100

    News model

--- SmartNewsApp/models.py
+++ SmartNewsApp/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 class Date(models.Model):
     date = models.CharField(max_length=50)
+
+class News(models.Model):
+    code = models.ForeignKey('Source', on_delete=models.CASCADE)
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    publishedAt = models.ForeignKey('Date', on_delete=models.CASCADE)
+    content = models.TextField()
+    topics = models.ManyToManyField('Topic', related_name='name')
