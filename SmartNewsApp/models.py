3d8aab2db946f4c7afdb7c034099c20063dc5a44
Robert <robert_torres@protonmail.com>
2020-03-09 14:00:56 +0100

    SourceModelv2

--- SmartNewsApp/models.py
+++ SmartNewsApp/models.py
@@ -4,11 +4,11 @@ class Date(models.Model):
     date = models.CharField(max_length=50)
 
 class Source(models.Model):
-    code = models.CharField(max_lenght = 25, help_text='Enter source identification')
-    name = models.CharField(max_lenght = 25)
+    code = models.CharField(max_length = 25, help_text='Enter source identification')
+    name = models.CharField(max_length = 25)
     description = models.TextField()
-    category = models.CharField(max_lenght = 20)
-    language = models.CharField(max_lenght = 4)
+    category = models.CharField(max_length = 20)
+    language = models.CharField(max_length = 4)
     country = models.ForeignKey('Country', on_delete=models.CASCADE)
 
 class Country(models.Model):
