b57752e940e79ea9081ee752811c2fd20c1b0f0b
ivancg98 <icortes.digital@gmail.com>
2020-03-09 13:55:56 +0100

    Models

--- SmartNewsApp/models.py
+++ SmartNewsApp/models.py
@@ -2,3 +2,15 @@ from django.db import models
 
 class Date(models.Model):
     date = models.CharField(max_length=50)
+
+class Country(models.Model):
+    country_code = models.CharField(max_length=25)
+
+class Topic(models.Model):
+    name = models.CharField(max_length=25)
+
+class Statistic(models.Model):
+    statistic_code = models.IntegerField()
+    concept = models.TextField()
+    date = models.ForeignKey('Date', on_delete=models.CASCADE)
+    name = models.ForeignKey('Topic', on_delete=models.CASCADE)
