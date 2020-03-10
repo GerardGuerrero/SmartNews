5cf7d0b5768165ac0bc805e8caf22031d1c1d016
Gerard Guerrero Lacasa <gerard@MacBook-MacBook-Pro-de-Gerard.local>
2020-03-10 10:49:48 +0100

    register models

--- SmartNewsApp/admin.py
+++ SmartNewsApp/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from apps.blog.models import News, Source, Topic, Country, Date, Statistic
 
-# Register your models here.
+class NewsAdmin(admin.ModelAdmin):
+ 	pass
+
+class SourceAdmin(admin.ModelAdmin):
+ 	pass
+
+class TopicAdmin(admin.ModelAdmin):
+ 	pass
+
+class CountryAdmin(admin.ModelAdmin):
+ 	pass
+
+class DateAdmin(admin.ModelAdmin):
+ 	pass
+
+class StatisticAdmin(admin.ModelAdmin):
+ 	pass
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Source, SourceAdmin)
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Date, DateAdmin)
+admin.site.register(Statistic, StatisticAdmin)
